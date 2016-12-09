10bd1505295f2a2dc71bb88cbfd1433d9c9b2f47
Cristian <Cristian@10.0.89.155>
2016-12-09 11:49:48 +0100

    start_abstract_machine added

--- Compilers/ARuntimeEnviroment.c
+++ Compilers/ARuntimeEnviroment.c
@@ -5,11 +5,34 @@
  *      Author: fabio & cristian
  */
 
-main(int argc, char *argv[]) {
-	Acode *instruction;
+extern Acode *program;
+extern int pc;
+Arecord **astack;
+Orecord **ostack;
+char *istack;
+int asize, osize, isize;
+int ap, op, ip;
+long allocated = 0,
+		deallocated = 0;
+
+
+
+main(int  argc,  char *argv[]) {
+	Acode *instruction;
 	start_abstract_machine();
-	while ((instruction = &program[pc++])->operator != HALT)
+	while ((instruction = &program[pc++])­>opertor != HALT)
 		execute(instruction);
 	stop_abstract_machine();
-	printf("Hello World");
+}
+
+void start_abstract_machine()
+{
+	load_acode();
+	pc = ap = op = ip = 0;
+	astack = (Arecord**) newmem(sizeof(Arecord*) * ASEGMENT);
+	asize = ASEGMENT;
+	ostack = (Orecord**) newmem(sizeof(Orecord*) * OSEGMENT);
+	osize = OSEGMENT;
+	istack = (char*) newmem(ISEGMENT);
+	isize = ISEGMENT;
 }
